d28fda8ccfe07f2e6d8adee222c141c8083ab2a3
VoIPMonk <shido6@gmail.com>
2024-01-26 09:53:29 -0500

    Update parkit11.py
    Commenting only found in Line Display branch

--- parkit11.py
+++ parkit11.py
@@ -76,7 +76,7 @@ xml_template = '''<?xml version="1.0" encoding="UTF-8"?>
     {% endfor %}
 </CiscoIPPhoneDirectory>
 '''
-
+#Boop Line Display Branch
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
