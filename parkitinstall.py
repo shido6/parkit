95ed4a8ffe3335f2757311862ec407c8047a5e2e
VoIPMonk <shido6@gmail.com>
2024-02-05 09:23:41 -0500

    Update parkitinstall.py

--- parkitinstall.py
+++ parkitinstall.py
@@ -110,16 +110,13 @@ for file_path in result.stdout.splitlines():
             xml_content = xml_file.read()
             
             # Use regular expressions to find and replace the <directoryURL> tag
-            #modified_xml_content = re.sub(r'<directoryURL>.*?</directoryURL>', f'<directoryURL>http://{current_ip}:5001/services</directoryURL>', xml_content)
             modified_xml_content = re.sub(r'<directoryURL>.*?</directoryURL>', '<directoryURL>http://%s:5001/services</directoryURL>' % current_ip, xml_content)
         
         # Save the modified content back to the XML file
         with open(file_path, 'w') as modified_file:
             modified_file.write(modified_xml_content)
 
-    #    print(f"Modified {file_path}")
-    #    print("Modified %s" % file_path)
-    #    print("Modified {}".format(file_path))
-    print("Modified " + file_path)
+        print("Modified {}".format(file_path))
     except Exception as e:
         print(f"Error processing {file_path}: {e}")
+        
