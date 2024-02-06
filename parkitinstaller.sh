8f568b6ac59da3b0e497bd225d83c6eade191830
VoIPMonk <shido6@gmail.com>
2024-02-05 19:34:19 -0500

    Update parkitinstaller.sh

--- parkitinstaller.sh
+++ parkitinstaller.sh
@@ -27,76 +27,35 @@ sudo ln -s /usr/bin/python3.6 /usr/bin/python3
 sudo python3 -m pip install --upgrade pip
 sudo yum update -y
 
+
+sudo su - 
+su - asterisk
+
 # Install prerequisites and set up a virtual environment
 echo "Create virtual environment myenv python"
 sudo python3 -m venv /home/asterisk/myenv
-sudo -u $current_user python3 -m venv /home/asterisk/myenv
 
 
 # Activate the virtual environment
 echo "Installing flask and pyst2"
-#sudo source $current_user_home/myenv/bin/activate
-source /home/asterisk/myenv/bin/activate
-#sudo /bin/bash -c "source /home/asterisk/myenv/bin/activate"
+sudo su - 
+su - asterisk
+source myenv/bin/activate
  pip install flask pyst2 asterisk
  deactivate
+exit
 
 echo "Making the scripts directory within /var/lib/asterisk"
 sudo mkdir -p /var/lib/asterisk/scripts
 echo "Copying script to /var/lib/asterisk/scripts"
 sudo cp -v $current_user_home/parkit/parkit11.py /var/lib/asterisk/scripts/parkit11.py
 
-#echo "Changing ownership and moving files to headless asterisk user"
-# Move the environment to the asterisk user folder
-#sudo tar -zcvf myenv.tar.gz $current_user_home/myenv/
-#sudo mv myenv.tar.gz /home/asterisk/
-
-# Extract myenv.tar.gz in /home/asterisk
-#echo "Extracting myenv.tar.gz in /home/asterisk"
-#cd /home/asterisk
-#sudo tar -zxvf myenv.tar.gz
-
-# Check if the myenv directory exists
-myenv_directory="$current_user_home/myenv"
-if [ ! -d "$myenv_directory" ]; then
-    echo "Error: The myenv directory does not exist."
-    exit 1
-fi
-
-# Move the myenv directory to /home/asterisk
-echo "Moving the myenv directory to /home/asterisk"
-sudo mv "$myenv_directory" /home/asterisk/
 
 # Verify that myenv is now in /home/asterisk
 if [ -d "/home/asterisk/myenv" ]; then
-    echo "Successfully moved myenv to /home/asterisk"
+    echo "Successfully updated myenv in /home/asterisk"
 else
-    echo "Error: Failed to move myenv to /home/asterisk."
-    exit 1
-fi
-
-# Create myenv.tar.gz
-echo "Creating myenv.tar.gz"
-sudo tar -zcvf /home/asterisk/myenv.tar.gz -C /home/asterisk myenv
-
-# Verify that myenv.tar.gz is now in /home/asterisk
-if [ -f "/home/asterisk/myenv.tar.gz" ]; then
-    echo "Successfully created myenv.tar.gz in /home/asterisk"
-else
-    echo "Error: Failed to create myenv.tar.gz in /home/asterisk."
-    exit 1
-fi
-
-# Extract myenv.tar.gz in /home/asterisk
-echo "Extracting myenv.tar.gz in /home/asterisk"
-cd /home/asterisk
-sudo tar -zxvf myenv.tar.gz
-
-# Verify that myenv is now extracted in /home/asterisk
-if [ -d "/home/asterisk/myenv" ]; then
-    echo "Successfully extracted myenv in /home/asterisk"
-else
-    echo "Error: Failed to extract myenv in /home/asterisk."
+    echo "Error: Failed to update myenv in /home/asterisk."
     exit 1
 fi
 
