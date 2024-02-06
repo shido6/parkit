ef55c48533677587073b97c055e97082c978892c
VoIPMonk <shido6@gmail.com>
2024-02-05 19:49:50 -0500

    Update parkitinstaller.sh

--- parkitinstaller.sh
+++ parkitinstaller.sh
@@ -64,7 +64,7 @@ Description=My Parked Calls Service
 After=network.target
 
 [Service]
-ExecStart="/home/asterisk/myenv/bin/python3 myenv /var/lib/asterisk/scripts/parkit11.py"
+ExecStart=/var/lib/asterisk/scripts/parkit11.py
 WorkingDirectory=/var/lib/asterisk/scripts
 Restart=always
 User=asterisk
